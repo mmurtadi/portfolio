ff565cb4f9696356dc487bb3057a4aabc440653c
mmurtadi <mmurtadi@gmail.com>
2017-01-12 04:08:32 -0500

    Website and portfolio update, enactus website also touched up

---
+++ AppDev/Diary/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$link= mysqli_connect("localhost", "cl21-oneserver", "eh7UJ4z^y", "cl21-oneserver");
+
+if (mysqli_connect_error()) {
+  die("Could NOt connect to database");
+}
+
+?>
---
+++ bootstrap/mmurtadi 2.0/diary/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$link= mysqli_connect("localhost", "cl43-murtadidb", "TqJMm/-U^", "cl43-murtadidb");
+
+if (mysqli_connect_error()) {
+  die("Could NOt connect to database");
+}
+
+?>
