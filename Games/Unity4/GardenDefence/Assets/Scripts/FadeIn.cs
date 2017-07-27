6553aabcaa19137d712cf253729fa7962deb1dc5
mmurtadi <mmurtadi@gmail.com>
2017-07-26 23:16:30 -0400

    bulk upload

---
+++ Games/Unity4/GardenDefence/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+	public float FadeInTimeInSeconds;
+
+	private Image fadePanel;
+	private Color currentColour = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad < FadeInTimeInSeconds){
+		float alphaCHange = Time.deltaTime / FadeInTimeInSeconds;
+			currentColour.a -= alphaCHange;
+			fadePanel.color = currentColour;
+		}
+		else{
+		gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Games/Unity4/GardenDefence/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		if(thisLevelMusic){
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+			
+		}
+
+	}
+
+	public void SetVolume(float volume){
+		audioSource.volume = volume;
+	}
+}
